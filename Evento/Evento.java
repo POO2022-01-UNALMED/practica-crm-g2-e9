e44b70890f37acc8def52dc131bd2431a71bf53e
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-30 18:59:54 -0500

    Update Evento.java

--- Evento/Evento.java
+++ Evento/Evento.java
@@ -1,70 +1,43 @@
-package gestionApp;
+package Evento;
+import gestionApp.Negocio;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Evento {
 
-    int ID;
-    Negocio negocio;
-    String tipoInteraccion;
-    String descripcion;
-    String fecha;
-    String respuesta;
+    protected int ID;
+    protected Negocio negocio;
+    protected String descripcion;
+    protected LocalDateTime fecha;
+    protected String respuesta;
 
-    public Evento(int ID, Negocio negocio, String tipoInteraccion, String descripcion){
+    public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+      
+    public Evento(int ID, Negocio negocio, String descripcion, LocalDateTime fecha){
         this.ID = ID;
         this.negocio = negocio;
-        this.tipoInteraccion = tipoInteraccion;
         this.descripcion = descripcion;
-        this.fecha = "00/00/0000";
-        this.respuesta = this.darRespuesta();
-
-        //ejecucion de metodo para dar respuesta;
+        this.fecha = fecha;
+        negocio.agregarEvento(this);
     }
-
-    private String darRespuesta(){
-        float probabilidadDeInteres = (float) 0.3;
-        if (negocio.getEtapa().equals("Presentación")){
-            float probabilidad =    negocio.getEmpleadoEncargado().getHabilidadVenta()+
-                    negocio.cliente.getProbCompra()+ probabilidadDeInteres;
-
-            if (probabilidad >= 0.7 ){return "Si";
-            } else if (probabilidad <= 0.3) {return "No";}
-            else {return "Interes";}
-        }
-
-        if (negocio.getEtapa().equals("Negociación")){
-            float valorEsperado = (float) (Math.random()*0.5+0.5);
-            if (valorEsperado >= 0.75){return "Compra";
-            }else if (valorEsperado >= 0.6 && valorEsperado <0.75){
-                return "No compra, sigue interesado";
-            }else {return "No compra";}
-        }
-
-
-
-        return "a";
-    }
-
 
     //---------------Metodos Setter y Getter---------------
 
-    public int getID() {return ID;}
-    public void setID(int ID) {this.ID = ID;}
+    protected int getID() {return ID;}
+    protected void setID(int ID) {this.ID = ID;}
 
-    public String getTipoInteraccion() {return tipoInteraccion;}
-    public void setTipoInteraccion(String tipoInteraccion) {this.tipoInteraccion = tipoInteraccion;}
+    protected Negocio getNegocio() {return negocio;}
+    protected void setNegocio(Negocio negocio) {this.negocio = negocio;}
 
-    public Negocio getNegocio() {return negocio;}
-    public void setNegocio(Negocio negocio) {this.negocio = negocio;}
+    protected String getDescripcion() {return descripcion;}
+    protected void setDescripcion(String descripcion) {this.descripcion = descripcion;}
 
-    public String getDescripcion() {return descripcion;}
-    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    protected LocalDateTime getFecha() {return fecha;}
+    protected void setFecha(LocalDateTime fecha) {this.fecha = fecha;}
 
-    public String getFecha() {return fecha;}
-    public void setFecha(String fecha) {this.fecha = fecha;}
-
-    public String getRespuesta() {return respuesta;}
-    public void setRespuesta(String respuesta) {this.respuesta = respuesta;}
-
+    protected String getRespuesta() {return respuesta;}
+    protected void setRespuesta(String respuesta) {this.respuesta = respuesta;}
 
     //Metodo para elegir si una respuesta cambia la etapa del negocio
 
@@ -81,11 +54,4 @@ public class Evento {
         
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hola!");
-        float a = (float) (Math.random()*0.4);
-        float valorEsperado = (float) (Math.random()*0.5+0.5);
-        System.out.println(valorEsperado);
-    }
-    
 }
