f7cfe1535616df49eeb64cc4f5f225d1c82e0718
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-30 18:59:57 -0500

    Create Llamada.java

---
+++ Evento/Llamada.java
@@ -0,0 +1,30 @@
+package Evento;
+import gestionApp.Negocio;
+import java.time.LocalDateTime;
+
+public class Llamada extends Evento{
+
+    double duracionLlamada;
+
+    public Llamada(int ID, Negocio negocio, String descripcion){
+        super(ID, negocio, descripcion, LocalDateTime.now());
+        this.duracionLlamada = Math.random();
+
+        this.darRespuesta();
+    }
+
+    private void darRespuesta(){
+        
+        if ((negocio.getEmpleadoEncargado().getHabilidadVenta() + negocio.cliente.getProbCompra() + this.duracionLlamada)/3> 0.7){
+            this.respuesta = "Interesado";
+        }
+        else{
+            this.respuesta = "No interesado";
+        }
+
+    }
+ 
+
+
+
+}
