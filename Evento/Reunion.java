3146a887c52ea920e97243cad7bcc045fe193146
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-30 19:00:00 -0500

    Create Reunion.java

---
+++ Evento/Reunion.java
@@ -0,0 +1,26 @@
+package Evento;
+import gestionApp.Negocio;
+
+import java.time.LocalDateTime;
+
+
+public class Reunion extends Evento{
+    
+    double duracionReunion;
+
+    public Reunion(int ID, Negocio negocio, String descripcion){
+        super(ID, negocio, descripcion, LocalDateTime.now());
+        this.duracionReunion = Math.random();
+        this.darRespuesta();
+    }
+
+    private void darRespuesta(){
+        
+        if ((negocio.getEmpleadoEncargado().getHabilidadVenta() + negocio.cliente.getProbCompra() + this.duracionReunion)/3> 0.7){
+            super.respuesta = "Interesado";
+        }
+        else{
+            super.respuesta = "No interesado";
+        }
+    }
+}
