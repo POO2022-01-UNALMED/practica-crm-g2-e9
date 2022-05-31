535d6d041f722e928dce4bf15ad85579c92eb21e
santiagoperez0 <jperezpi@unal.edu.co>
2022-05-30 22:32:42 -0500

    sssss

--- gestionApp/servicios/Registro.java
+++ gestionApp/servicios/Registro.java
@@ -8,28 +8,26 @@ import gestionApp.servicios.Servicio;
 import java.util.ArrayList;
 
 public class Registro {
-	private static ArrayList<Empleado> empleados= new ArrayList<>();
-	private static ArrayList<Cliente> clientes= new ArrayList<>();
+	private static ArrayList<Empleado> empleados= new ArrayList<Empleado>();
+	private static ArrayList<Cliente> clientes= new ArrayList<Cliente>();
 	private static ArrayList<Empresa> empresas= new ArrayList<>();
-	private static ArrayList<Servicio> servicios= new ArrayList<>();
+	private static ArrayList<Servicio> servicios= new ArrayList<Servicio>();
 	
 	
 	//registros 
    
-    // public  Empleado registrarEmpleado(String nombre, String apellido, int cedula, int edad, String correo, int celular, String fechaInicioContrato) {
-    // 	Empleado empleado= new Empleado (nombre,apellido,cedula, edad,correo,celular,fechaInicioContrato);
-    // 	empleados.add(empleado);
-    // 	return empleado;
-    // }
+    public static Empleado registrarEmpleado(String nombre, int cedula, String cel, String correo, int edad, String cargo, String fechaInicioContrato) {
+     	Empleado empleado= new Empleado(nombre,cedula,cel ,correo,edad,cargo,fechaInicioContrato);
+    	empleados.add(empleado);
+     	return empleado;
+    }
     
-
-    //por que registrar cliente devuelve un cliente?
-
-    public  Cliente registrarCliente(String nombre, int cedula, String cel, String correo, int id, Empresa empresa, String cargo, Boolean activo) {
+    
+    public static Cliente registrarCliente(String nombre, int cedula, String cel, String correo, int id, Empresa empresa, String cargo, Boolean activo) {
     	Cliente cliente = new Cliente (nombre, cedula, cel, correo, id, empresa, cargo, activo);
     	clientes.add(cliente);
     	return  cliente;
-        }//x2??
+    }
     public  Empresa registrarEmpresa(String nombre, int nit, String descripcion){
         Empresa empresa= new Empresa (nombre, nit, descripcion);
         empresas.add(empresa);
