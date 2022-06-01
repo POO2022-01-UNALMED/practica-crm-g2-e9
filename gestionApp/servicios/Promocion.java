cec0a690383378cbe0d52c11c3997a4e0acdab04
santiagoperez0 <jperezpi@unal.edu.co>
2022-06-01 00:56:19 -0500

    Modificiacion de Promocion y Servicio
    Si la cago la pago

--- gestionApp/servicios/Promocion.java
+++ gestionApp/servicios/Promocion.java
@@ -6,14 +6,16 @@ public class Promocion {
 	private String id;
 	private double descuento;
 	private ArrayList<Servicio> Servicios = new ArrayList<Servicio>();
+	private ArrayList<Promocion> allPromocion = new ArrayList<>();
 
 	private static int idCounter = 0;
 
-	public Promocion(int id, double descuento, ArrayList<Servicio> servicios) {
+	public Promocion(double descuento, Servicio servicio) {
 		this.id = "0".repeat(3-String.valueOf(idCounter).length()) + String.valueOf(idCounter);
 		this.descuento = descuento;
-		Servicios = servicios;
 		idCounter += 1;
+		allPromocion.add(this);
+		servicio.addPromocion(this);
 	}
 	
 	
