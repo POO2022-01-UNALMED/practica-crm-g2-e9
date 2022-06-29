1fb661f2f1c047e5b2b653bad0c83b1917950c6a
Altraoz <curresty@unal.edu.co>
2022-06-28 22:46:04 -0500

    a
    a

--- Python/gestionApp/Personas/Cliente.py
+++ Python/gestionApp/Personas/Cliente.py
@@ -1,4 +1,6 @@
-from Python.gestionApp.Personas import Persona
+from random import uniform
+
+from Python.gestionApp.Personas.Persona import Persona
 
 
 class Cliente(Persona):
@@ -8,12 +10,12 @@ class Cliente(Persona):
     listaEventos = []
     idCounter = 0
     
-    def __init__(self, nombre, cedula, cel, correo, id, cargo, activo, probCompra ):
+    def __init__(self, nombre, cedula, cel, correo, id, cargo, activo ):
         super().__init__(nombre, cedula, cel, correo)
         self.id = id
         self.cargo = cargo
         self.activo = activo
-        self.probCompra = probCompra
+        self.probCompra = uniform(0, 0.4)
     
     @classmethod
     def getId(self): return self.id
--- Python/gestionApp/servicios/a.py
+++ Python/gestionApp/servicios/a.py
@@ -1,3 +1,4 @@
+from Python.gestionApp.Personas.Cliente import Cliente
 from Python.gestionApp.Personas.Empleado import Empleado
 from Python.gestionApp.Personas.Persona import Persona
 
@@ -5,4 +6,5 @@ if __name__ == "__main__":
     persona1 = Persona("Carlos",100,310,"carlos@unal.edu.co")
     print(persona1.nombre)
 
-    empleado1 = Empleado("Carlos", 100, 310, "carlos@unal.edu.co", "recogeCucarachas", "10/10/2010")
+    #empleado1 = Empleado("Carlos", 100, 310, "carlos@unal.edu.co", "recogeCucarachas", "10/10/2010")
+    cliente1 = Cliente("Adres", 101, 311, "andres@unal.edu.co", 1001, "Cajero", True)
